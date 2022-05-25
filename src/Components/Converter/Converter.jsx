43a43d459b986a9a99f9aa1fb317c4254327e6ce
elogeek <elodiechristin@gmail.com>
2022-05-25 14:28:13 +0200

    debugg project

--- src/Components/Converter/Converter.jsx
+++ src/Components/Converter/Converter.jsx
@@ -1,26 +1,66 @@
 import "./Converter.css";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faArrowRightArrowLeft} from '@fortawesome/free-solid-svg-icons';
+import {useState} from "react";
 
-export const Converter = function({units,setConverter}) {
+export const Converter = function() {
+    /**
+     * For to have it easier to manage the fields to convert
+     */
+    const [entry, setEntry] = useState(1);
+    const [from, setFrom] = useState(0);
+    const [to, setTo] = useState(0);
+    const [unit, setUnit] = useState(0);
+
+    const [result, setResult] = useState("");
 
     /**
-     * Check action click convert
+     * For to have it easier to manage units of measure and liter
+     */
+    const values = [
+        {
+            id: 0,
+            name: "valeur",
+            base: 1,
+        },
+        {
+            id: 1,
+            name: "deci",
+            base: 0.1,
+        },
+        {
+            id: 2,
+            name: "centi",
+            base: 0.01,
+        },
+        {
+            id: 3,
+            name: "mili",
+            base: 0.001,
+        },
+    ];
+
+    /**
+     * For to have it easier to manage units of measure and liter
+     */
+    const units = [
+        {
+            id: 0,
+            title: "metre",
+        },
+        {
+            id: 1,
+            title: "litre"
+        }
+    ];
+
+    /**
+     * Check and manage action click convert
      * @param e
      */
     function handleClickConvert(e) {
-        units
-            .filter(unit => units === 0 || units === units)
-            .map(units => console.log("here result test : " + units.unitStart + " vaut " + units.unitConverter));
-    }
-
-    /**
-     * Refresh the page of the site web
-     * @param e
-     */
-    function handleClick(e) {
-        units.map(unit => unit.unitStart === 0);
-        setConverter(true);
+        let res = (values[to].base / values[from].base) * entry;
+        setResult(res.toString());
     }
 
     return(
@@ -29,50 +69,58 @@ export const Converter = function({units,setConverter}) {
 
                 <div>
                     <label htmlFor="input-amount">Montant</label>
-                    <input type="number" name="input-amount" min="0"/>
+                    <input type="number" name="input-amount" value={entry} onChange={e => setEntry(parseInt(e.target.value))}/>
                 </div>
 
                 <div>
+                    {/* Field for amount to be converted */}
                     <label htmlFor="input-amount-device">Chossissez votre unité :</label>
                     <select
                         name="input-amount-device"
                         id="input-amount-device"
-                        onChange={(e) => setConverter(e.target.value)}
-                    >
-                        <option value="m">mètre</option>
-
+                        onChange={e => setFrom(parseInt(e.target.value))}>
+                        {values.map(converter => <option key={converter.id} value={converter.id}>{converter.name}</option>)}
                     </select>
-                </div>
-
-                <div>
-                    <button type="button"><FontAwesomeIcon icon={faArrowRightArrowLeft}/></button>
-                </div>
-
-                <div>
-                    <label htmlFor="input-convert">Convertir l'unité en :</label>
+                    {/* Field for choosing conversion units */}
                     <select
                         name="input-convert"
                         id="input-convert"
                         readonly="true"
-                        onChange={(e) => setConverter(e.target.value)}>
-                    >
-                        <option value="m">mètre</option>
-                        <option value="dm">décimètre</option>
-                        <option value="cm">centimètre</option>
-                        <option value="mm">millimètre</option>
+                        onChange={e => setUnit(parseInt(e.target.value))}>
+                        {units.map(unit => <option key={unit.id} value={unit.id}>{unit.title}</option>)}
                     </select>
                 </div>
 
+                <div id="btnArrow">
+                    <button type="button"><FontAwesomeIcon icon={faArrowRightArrowLeft}/></button>
+                </div>
+
                 <div className="result">
-                    Le résultat est : {units.unitStart} vaut {units.unitConverter} !
+                    <label htmlFor="input-convert">Convertir l'unité en : </label>
+                    <select onChange={e => setTo(parseInt(e.target.value))}>
+                        {values.map(converter => <option key={converter.id} value={converter.id}>{converter.name}</option>)}
+                    </select>
+                    Le résultat est : {result.toString() + " " + (values[to].id !== 0 ? values[to].name: "") + units[unit].title + (result > 1 ? "s" : "")} !
                 </div>
 
                 <div>
                     <button type="submit" onClick={handleClickConvert}>Convertir</button>
-                    <button type="submit" onClick={handleClick}>Reset</button>
+
+                    <button type="submit"
+                       onClick={
+                            ()=> {
+                                setEntry(0);
+                                setFrom(0);
+                                setTo(0);
+                                setResult("");
+                                setUnit(0);
+                            }
+                        }>Reset
+                    </button>
                 </div>
 
             </form>
         </div>
     );
 };
+
